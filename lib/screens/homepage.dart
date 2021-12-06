246dd8eab7acaa76be53b6f7c450df090cc46f18
binhlecong <congbinh21012001@gmail.com>
2021-12-06 23:13:29 +0700

    add - floating action button

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'taskpage.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -18,8 +20,32 @@ class _HomePageState extends State<HomePage> {
           color: const Color(0xFFF6F6F6),
           child: Stack(
             children: [
-              SizedBox(
-                height: 10,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 100,
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                bottom: 24.0,
+                right: 0.0,
+                child: FloatingActionButton(
+                  child: const Icon(
+                    Icons.add,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => TaskPage()),
+                    ).then((value) {
+                      setState(() {});
+                    });
+                  },
+                ),
               )
             ],
           ),
