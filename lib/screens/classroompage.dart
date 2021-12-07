7c793f22d19d3dd0e99a6a4e2413c8b237f8b689
Gideon2801 <quando2801@gmail.com>
2021-12-07 21:22:58 +0700

    update - update classroompage.dart

--- lib/screens/classroompage.dart
+++ lib/screens/classroompage.dart
@@ -13,8 +13,11 @@ class ClassroomPage extends StatefulWidget {
 }
 
 class _ClassroomPageState extends State<ClassroomPage> {
+  final _title = TextEditingController();
+  final _url = TextEditingController();
+  final _notify = TextEditingController();
+  final _description = TextEditingController();
   DatabaseHelper _databaseHelper = DatabaseHelper();
-  final myController = TextEditingController();
   final format = DateFormat("yyyy-MM-dd HH:mm");
   Classroom? classroom = null;
 
@@ -34,163 +37,150 @@ class _ClassroomPageState extends State<ClassroomPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: null,
+            icon: const Icon(
+              Icons.qr_code_scanner_outlined,
+              size: 35,
+            ),
+            tooltip: 'Saved Suggestions',
+          )
+        ],
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () async {
             Navigator.pop(context);
           },
         ),
+        title: const Text('Notify'),
       ),
       body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.fromLTRB(10, 30, 10, 30),
-          child: ListView(
-            children: [
-              TextField(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            const SizedBox(height: 40.0),
+            DateTimeField(
+              decoration: InputDecoration(
+                labelText: 'Date & Time',
+                isDense: true,
+                contentPadding: const EdgeInsets.all(2),
+              ),
+              format: format,
+              onShowPicker: (context, currentValue) async {
+                final date = await showDatePicker(
+                    context: context,
+                    firstDate: DateTime(1900),
+                    initialDate: currentValue ?? DateTime.now(),
+                    lastDate: DateTime(2100));
+                if (date != null) {
+                  final time = await showTimePicker(
+                      context: context,
+                      initialTime: TimeOfDay.fromDateTime(
+                        currentValue ?? DateTime.now(),
+                      ));
+
+                  classroom!.dateTime = DateTimeField.combine(date, time);
+                  return DateTimeField.combine(date, time);
+                } else {
+                  return currentValue;
+                }
+              },
+            ),
+            const SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                buildDaySelector('Mo', 0),
+                buildDaySelector('Tu', 1),
+                buildDaySelector('We', 2),
+                buildDaySelector('Th', 3),
+                buildDaySelector('Fr', 4),
+                buildDaySelector('Sa', 5),
+                buildDaySelector('Su', 6),
+              ],
+            ),
+            const SizedBox(height: 10.0),
+            TextField(
+                autofocus: true,
+                controller: _title,
                 decoration: const InputDecoration(
-                  hintStyle: TextStyle(fontSize: 17),
-                  hintText: 'title',
-                  contentPadding: EdgeInsets.all(20),
+                  prefixIcon: Icon(Icons.topic_outlined),
+                  filled: true,
+                  labelText: 'Tile',
                 ),
                 onChanged: (value) {
                   classroom!.title = value;
-                },
-                controller: TextEditingController()
-                              ..text = classroom!.title!,
+                }),
+            const SizedBox(height: 15.0),
+            TextField(
+              autofocus: true,
+              controller: _url,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.link_rounded),
+                filled: true,
+                labelText: 'URL',
               ),
-              TextField(
-                decoration: const InputDecoration(
-                  hintStyle: TextStyle(fontSize: 17),
-                  hintText: 'description',
-                  contentPadding: EdgeInsets.all(20),
+              onChanged: (value) {
+                classroom!.url = value;
+              },
+            ),
+            const SizedBox(height: 15.0),
+            TextField(
+              autofocus: true,
+              controller: _notify,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.notifications_active_outlined),
+                filled: true,
+                labelText: 'Notify',
+                helperText: 'Alarm before minutes',
+              ),
+              onChanged: (value) {
+                classroom!.alarmBefore = int.tryParse(value) ?? 1;
+              },
+            ),
+            const SizedBox(height: 15.0),
+            TextField(
+              autofocus: true,
+              maxLines: null,
+              controller: _description,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.description_outlined),
+                filled: true,
+                labelText: 'Description',
+              ),
+              onChanged: (value) {
+                classroom!.description = value;
+              },
+            ),
+            const SizedBox(height: 100.0),
+            ButtonBar(
+              children: <Widget>[
+                TextButton(
+                  child: const Text('DELETE'),
+                  onPressed: () async {
+                    if (widget.classroom != null) {
+                      await _databaseHelper.deleteClassroom(classroom!.id!);
+                    }
+                    Navigator.pop(context);
+                  },
                 ),
-                onChanged: (value) {
-                  classroom!.description = value;
-                },
-                controller: TextEditingController()
-                              ..text = classroom!.description!,
-              ),
-              TextField(
-                decoration: const InputDecoration(
-                  hintStyle: TextStyle(fontSize: 17),
-                  hintText: 'alarm before minutes:',
-                  contentPadding: EdgeInsets.all(20),
+                ElevatedButton(
+                  child: const Text('SAVE'),
+                  onPressed: () async {
+                    int _id = 0;
+                    if (widget.classroom == null) {
+                      _id = await _databaseHelper.insertClassroom(classroom!);
+                    } else {
+                      _id = await _databaseHelper.updateClassroom(classroom!);
+                    }
+                    Navigator.pop(context);
+                    //
+                  },
                 ),
-                onChanged: (value) {
-                  classroom!.alarmBefore = int.tryParse(value) ?? 1;
-                },
-                controller: TextEditingController()
-                              ..text = classroom!.alarmBefore!.toString(),
-              ),
-              DateTimeField(
-                decoration: InputDecoration(
-                  labelText: classroom!.dateTime!.toIso8601String(),
-                  isDense: true,
-                  contentPadding: const EdgeInsets.all(2),
-                ),
-                format: format,
-                onShowPicker: (context, currentValue) async {
-                  final date = await showDatePicker(
-                      context: context,
-                      firstDate: DateTime(1900),
-                      initialDate: currentValue ?? DateTime.now(),
-                      lastDate: DateTime(2100));
-                  if (date != null) {
-                    final time = await showTimePicker(
-                        context: context,
-                        initialTime: TimeOfDay.fromDateTime(
-                          currentValue ?? DateTime.now(),
-                        ));
-
-                    classroom!.dateTime = DateTimeField.combine(date, time);
-                    return DateTimeField.combine(date, time);
-                  } else {
-                    return currentValue;
-                  }
-                },
-              ),
-              Row(
-                children: [
-                  buildDaySelector('Mon', 0),
-                  buildDaySelector('Tue', 1),
-                  buildDaySelector('Wed', 2),
-                  buildDaySelector('Thu', 3),
-                  buildDaySelector('Fri', 4),
-                  buildDaySelector('Sat', 5),
-                  buildDaySelector('Sun', 6),
-                ],
-              ),
-              TextField(
-                decoration: const InputDecoration(
-                  hintStyle: TextStyle(fontSize: 17),
-                  hintText: 'url',
-                  contentPadding: EdgeInsets.all(20),
-                ),
-                onChanged: (value) {
-                  classroom!.url = value;
-                },
-                controller: TextEditingController()
-                              ..text = classroom!.url!,
-              ),
-              TextField(
-                decoration: const InputDecoration(
-                  hintStyle: TextStyle(fontSize: 17),
-                  hintText: 'importance',
-                  contentPadding: EdgeInsets.all(20),
-                ),
-                onChanged: (value) {
-                  classroom!.importance = int.tryParse(value) ?? 1;
-                },
-                controller: TextEditingController()
-                              ..text = classroom!.importance!.toString(),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: TextButton.icon(
-                            icon: const Icon(Icons.delete),
-                            label: const Text('Delete'),
-                            onPressed: () async {
-                              if (widget.classroom != null) {
-                                await _databaseHelper
-                                    .deleteClassroom(classroom!.id!);
-                              }
-                              Navigator.pop(context);
-                            },
-                          ),
-                        ),
-                        Expanded(
-                          child: TextButton.icon(
-                            icon: const Icon(
-                              Icons.check,
-                            ),
-                            label: const Text('Save'),
-                            onPressed: () async {
-                              int _id = 0;
-                              if (widget.classroom == null) {
-                                _id = await _databaseHelper
-                                    .insertClassroom(classroom!);
-                              } else {
-                                _id = await _databaseHelper
-                                    .updateClassroom(classroom!);
-                              }
-
-                              Navigator.pop(context);
-                            },
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
@@ -204,20 +194,22 @@ class _ClassroomPageState extends State<ClassroomPage> {
           classroom!.setRepeatAt(value, !isRepeated);
         });
       },
-      child: SizedBox(
-        width: 50,
-        height: 40,
-        child: Text(
-          name,
-          style: TextStyle(color: isRepeated ? Colors.red : Colors.grey),
+      child: SizedBox.fromSize(
+        size: Size.fromRadius(20.0),
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: new BorderRadius.circular(20.0),
+              color: isRepeated ? Colors.lightBlue : Colors.white70),
+          child: Center(
+              child: Text(
+            name,
+            style: TextStyle(
+              fontSize: 18,
+              color: isRepeated ? Colors.white : Colors.black,
+            ),
+          )),
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    // TODO: dispose focus node if needed
-    super.dispose();
   }
 }
